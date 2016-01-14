e2506147b1819f770e5034a6f22228f78e341c43
JR-one <j.rivoalan@gmail.com>
2016-01-14 16:06:34 +0100

    Test n2

---
+++ application/views/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>403 Forbidden</title>
+    <link rel="stylesheet" href=" <?php echo base_url("assets/css/bootstrap.css"); ?> " />
+</head>
+<body>
+
+<p>Directory access is forbidden.</p>
+
+<script type="text/javascript" src="<?php echo base_url("assets/js/jQuery-1.10.2.js"); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
+</body>
+</html>
